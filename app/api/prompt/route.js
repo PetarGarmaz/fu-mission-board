9b48f2dbe830fc255a43b26eed9d713a6ab7ee14
Petar Garmaz <petar.garmaz@gmail.com>
2023-12-07 21:06:47 +0100

    Attempt to fix broken feed api #3

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,8 +6,17 @@ export const GET = async (req) => {
 		await connectToDB();
 
 		const briefings = await Briefing.find().populate("host");
+		const res = Response(JSON.stringify(briefings), {status:200});
 
-		return new Response(JSON.stringify(briefings), {status:200})
+		// Add a unique identifier to the URL to force a cache-busting reload
+        const url = new URL(req.url);
+        url.searchParams.set("t", Date.now());
+        res.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+        res.headers.set("Pragma", "no-cache");
+        res.headers.set("Expires", "0");
+        res.headers.set("Location", url.toString());
+
+        return res;
 	} catch (error) {
 		return new Response("FAIL", {status:500})
 	}
